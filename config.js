32d0ec3408da5db3e367d15a800563c4b76c6705
ducdx2002 <85302281+ducdx2002@users.noreply.github.com>
2021-06-04 00:32:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    introTitle: 'Lợn à!',
+    introTitle: 'Á à!',
     introDesc: `Ủa lại bị Đức lừa chui vô đây chứ gì :))`,
     btnIntro: 'Ấn thử xem',
     title: ' Có lẽ là anh nên nói: ',
-    desc: ' Một vài điều từ đôi môi, chỉ cần em thôi ',
+    desc: ' Một vài điều từ đôi môi, chỉ cần em thôi :)) ',
     btnYes: ' Yêu thế <3',
     btnNo: ' Cần db cc -.-',
     question:'Why ấn vô đây haha',
